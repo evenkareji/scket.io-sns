8825dea7116c72c743a4312631a37d9f307b6715
github user name <yokota.tomo222282@gmail.com>
2023-09-11 22:19:06 +0900

    リアルタイムチャット完成

--- socket/index.js
+++ socket/index.js
@@ -6,13 +6,43 @@ const io = require('socket.io')(8900, {
 
 let users = [];
 
+// soketIdとuser._idをセットにして入れる。user._idから特定のsocketIdをもつuserを検索することができる
+const addUser = (userId, socketId) => {
+  // some　配列が条件を一つでも満たしていればtrue
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
 io.on('connection', (socket) => {
+  // 接続した時
   console.log('a user connected.');
-  // すべてのユーザーに送信の場合
-  // io.emit('welcome', 'hello this is socket server');
-  // 1人のユーザーに送信の場合
-  io.to(si).emit('welcome', 'hello this is socket server');
 
   // クライアントから情報を取得する場合、引数のsocketを取得する
-  socket.on('addUser', (userId) => {});
+  socket.on('addUser', (userId) => {
+    addUser(userId, socket.id);
+    io.emit('getUsers', users);
+  });
+
+  // send and get message
+  socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+    // receiverIdから受信者のsocket.idを取得
+    const user = getUser(receiverId);
+
+    // 受信者を特定しclientへ送信
+    io.to(user.socketId).emit('getMessage', { senderId, text });
+  });
+
+  // 切断する時;
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
+    removeUser(socket.id);
+    io.emit('getUsers', users);
+  });
 });
